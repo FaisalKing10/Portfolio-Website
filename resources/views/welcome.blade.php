4d8ff9c469a3927ea34876b53ecb60ed44fef4ef
faisal_10 <r.f.imtiaz@gmail.com>
2024-09-12 17:10:16 +0800

    html and css

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,13 +4,28 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Portfolio Creator</title>
+        <link rel="stylesheet" href="/home/ubuntu/portfolio-website/resources/css/app.css">
 
         @livewireStyles
     </head>
+
+
     <body class="antialiased">
         <!-- Include Livewire Component -->
         <livewire:Portfolio-Component />
-
         @livewireScripts
+
+        <section class="Signup_cred">
+            <div class="auth_container">
+                <form>
+                    <label class="email">Email</label><br>
+                    <input class="input_pass" type="text"><br>
+                    <label class="password">Password</label><br>
+                    <input class="input_pass" type="password"><br>
+                    <input class="click_signup" type="button" value="SignUp"><br>
+                </form>
+            </div>
+        </section>
+
     </body>
 </html>
