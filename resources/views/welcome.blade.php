be6edf76222177a0f537d0244fe6ed720c7dd878
faisal_10 <r.f.imtiaz@gmail.com>
2024-09-13 17:41:56 +0800

    friday changes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Portfolio Creator</title>
-        <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="css/style.css">
 
         @livewireStyles
     </head>
@@ -15,14 +15,21 @@
         <livewire:Portfolio-Component />
         @livewireScripts
 
+        <?php
+        echo "WELCOME!";
+        ?>
+
         <section class="Signup_cred">
             <div class="auth_container">
                 <form>
-                    <label class="email">Email</label><br>
-                    <input class="input_pass" type="text"><br>
-                    <label class="password">Password</label><br>
-                    <input class="input_pass" type="password"><br>
-                    <input class="click_signup" type="button" value="SignUp"><br>
+                    E-mail: 
+                    <input class="input_EMAIL" type="text" name="Email"><br>
+                    Passowrd:
+                    <input class="input_pass" type="password">
+                    <br>
+                    <input class="byn_signup" type="button" value="SignUp">
+                    <br>
+                    <input class="btn_login" type="button" value="Login">
                 </form>
             </div>
         </section>
